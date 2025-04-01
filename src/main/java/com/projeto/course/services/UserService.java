662ebfc7563d9f949fd6c1405044b5682a0ae181
Marcos Ribeiro <mmota5674@gmail.com>
2025-04-01 18:09:09 -0300

    Exception handler - delete

--- src/main/java/com/projeto/course/services/UserService.java
+++ src/main/java/com/projeto/course/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.projeto.course.entities.User;
 import com.projeto.course.repositories.UserRepository;
+import com.projeto.course.services.exceptions.DataBaseException;
 import com.projeto.course.services.exceptions.ResourceNotFoundException;
 
 import jakarta.transaction.Transactional;
@@ -32,7 +35,13 @@ public class UserService {
 	}
 	
 	public void delete(Long id) {
+		try {
 		repository.deleteById(id);
+		}catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		}catch(DataIntegrityViolationException e) {
+			throw new DataBaseException(e.getMessage());
+		}
 	}
 	
 	@Transactional
