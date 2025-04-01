9599031abf8183cb4de35c7e3ec6326f6bae6774
Marcos Ribeiro <mmota5674@gmail.com>
2025-04-01 12:17:54 -0300

    OrderItem, many-to-many association with extra attributes PART 2 + jsonIgnore corrections (fetch eager)

--- src/main/java/com/projeto/course/entities/Product.java
+++ src/main/java/com/projeto/course/entities/Product.java
@@ -5,9 +5,8 @@ import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -30,8 +29,7 @@ public class Product implements Serializable {
 	private Double price;
 	private String imgUrl;
 
-	@JsonIgnore
-	@ManyToMany
+	@ManyToMany (fetch = FetchType.EAGER)
 	@JoinTable(name = "tb_product_category", joinColumns = @JoinColumn(name = "product_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
 	private Set<Category> categories = new HashSet<>();
 
